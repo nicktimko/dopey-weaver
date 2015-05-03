e6b203339b2548916af3f8cb55ac6da142be681b
Nick Timkovich <prometheus235@gmail.com>
2015-05-03 16:03:53 -0500

    Try to dock with something.

--- provision.sh
+++ provision.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-sudo apt-get -qy install git libdb-dev build-essential libz-dev libreadline5-dev libncursesw5-dev libssl-dev libgdbm-dev libsqlite3-dev libbz2-dev liblzma-dev libc6-dev tk-dev
+# Python deps and other stuff
+apt-get -qy install \
+    build-essential git linux-image-extra-$(uname -r) \
+    libdb-dev libz-dev libreadline-dev \
+    libncursesw5-dev libssl-dev libgdbm-dev \
+    libsqlite3-dev libbz2-dev liblzma-dev \
+    libc6-dev tk-dev
 
+# Docker
+wget -qO- https://get.docker.com/ | sh
+usermod -aG docker vagrant
+
+# Quality of life
 # blue dir color on blue-ish powershell background is impossible
 echo 'LS_COLORS=$LS_COLORS:"di=0;35:"' >> ~/.bashrc
 echo 'LS_COLORS=$LS_COLORS:"di=0;35:"' >> /home/vagrant/.bashrc
 
+# Pyenv
 curl -s -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | su vagrant -s /bin/bash
 
 cat <<EOF >> /home/vagrant/.bashrc
