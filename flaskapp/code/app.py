e19ccab96311b71543d0b4b459197f5992daa8d1
Nick Timkovich <prometheus235@gmail.com>
2015-05-04 01:18:35 -0500

    It's (Docker) alive and talking to the host (the super-host)

--- flaskapp/code/app.py
+++ flaskapp/code/app.py
@@ -1,9 +1,12 @@
+import time
 from flask import Flask
 app = Flask(__name__)
 
+t_launch = time.time()
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World!  (Uptime: {:.1f} seconds)".format(time.time() - t_launch)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", debug=True)
--- provision.sh
+++ provision.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Python deps and other stuff
-apt-get -qy install \
+apt-get -qq update
+apt-get -qqy install \
     build-essential git linux-image-extra-$(uname -r) \
     libdb-dev libz-dev libreadline-dev \
     libncursesw5-dev libssl-dev libgdbm-dev \
@@ -12,17 +13,20 @@ apt-get -qy install \
 wget -qO- https://get.docker.com/ | sh
 usermod -aG docker vagrant
 
+# Pyenv
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> /home/vagrant/.bashrc
+
+PYENV_INSTALLER="https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer"
+curl -s -L $PYENV_INSTALLER | su vagrant -s /bin/bash
+
+cat <<EOF >> /home/vagrant/.bashrc
+eval "\$(pyenv init -)"
+eval "\$(pyenv virtualenv-init -)"
+EOF
+
 # Quality of life
 # blue dir color on blue-ish powershell background is impossible
 echo 'LS_COLORS=$LS_COLORS:"di=0;35:"' >> ~/.bashrc
 echo 'LS_COLORS=$LS_COLORS:"di=0;35:"' >> /home/vagrant/.bashrc
-
-# Pyenv
-curl -s -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | su vagrant -s /bin/bash
-
-cat <<EOF >> /home/vagrant/.bashrc
-export PATH="\$HOME/.pyenv/bin:\$PATH"
-eval "\$(pyenv init -)"
-eval "\$(pyenv virtualenv-init -)"
-cd /vagrant
-EOF
+# start where our code is.
+echo 'cd /vagrant' >> /home/vagrant/.bashrc
