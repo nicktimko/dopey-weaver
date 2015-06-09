6064832448ba5a7c26478644ebef66d35d1eb72b
Nick Timkovich <prometheus235@gmail.com>
2015-06-08 19:58:37 -0500

    Replace skeleton project with Wagtail template

--- wapp/src/manage.py
+++ wapp/src/manage.py
@@ -3,10 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    import time
-    time.sleep(4) # TODO: fix this dirty hack to let the database start up
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myprj.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
     from django.core.management import execute_from_command_line
 
